c6728f5fc4a4fb2995d4561f0b5ec052d2315807
Daniele Vezzoli <daniele.vezzoli@lightone.eu>
2024-10-18 15:17:43 +0200

    Remove omnisharp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -30,8 +30,7 @@ return {
             ensure_installed = {
                 "lua_ls",
                 "gopls",
-                "omnisharp",
-                "marksman"
+                "marksman",
             },
             handlers = {
                 function(server_name) -- default handler (optional)
