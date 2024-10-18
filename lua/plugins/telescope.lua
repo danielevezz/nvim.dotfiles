a6248a04b1e4f80c73cc80ea31704d90e57cf8c1
Daniele Vezzoli <daniele.vezzoli@lightone.eu>
2024-10-18 15:09:44 +0200

    Telescope buffers

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,6 +5,7 @@ return {
 		keys = {
 			{'<leader>ff', "<cmd>Telescope find_files<cr>", desc = "Find file"},
 			{'<leader>fg', "<cmd>Telescope live_grep<cr>", desc = "Live grep"},
+			{'<leader>fb', "<cmd>Telescope buffers<cr>", desc = "Find buffers"},
 			{'<C-p>', "<cmd>Telescope git_files<cr>", desc = "Git files"}
 		}
 	}
